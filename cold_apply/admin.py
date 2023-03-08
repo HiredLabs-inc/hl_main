3a0413e72f82dc4dce7b005173d6087f978b8220
Jeff Stock <jantonstock@gmail.com>
2023-03-08 10:22:46 -0800

    updated admin interface

--- cold_apply/admin.py
+++ cold_apply/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Participant, Interaction, Job
+
+
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = (
+    'name', 'email', 'phone', 'veteran', 'active', 'current_phase', 'created_at', 'created_by', 'updated_at',
+    'updated_by')
+    list_filter = (
+    'name', 'email', 'phone', 'veteran', 'active', 'current_phase', 'created_at', 'created_by', 'updated_at',
+    'updated_by')
+    search_fields = (
+    'name', 'email', 'phone', 'veteran', 'active', 'current_phase', 'created_at', 'created_by', 'updated_at',
+    'updated_by')
+
+
+class InteractionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'participant', 'interaction_type', 'notes', 'created_at', 'updated_at')
+    list_filter = ('user', 'participant', 'interaction_type', 'notes', 'created_at', 'updated_at')
+    search_fields = ('user', 'participant', 'interaction_type', 'notes', 'created_at', 'updated_at')
+
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'created_at', 'updated_at')
+    list_filter = ('title', 'description', 'status', 'created_at', 'updated_at')
+    search_fields = ('title', 'description', 'status', 'created_at', 'updated_at')
+
+
+admin.site.register(Participant)
+admin.site.register(Interaction)
+admin.site.register(Job)
--- resume/admin.py
+++ resume/admin.py
@@ -1,8 +1,69 @@
 from django.contrib import admin
 
-from .models import Overview, Organization, Position, Degree,\
- Concentration, Experience, Bullet, Education, CertProjectActivity,\
+from .models import Overview, Organization, Position, Degree, \
+ Concentration, Experience, Bullet, Education, CertProjectActivity, \
  LanguageFATS
+
+
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('start_date', 'end_date', 'label', 'org', 'position')
+    list_filter = ('label', 'org', 'position')
+    search_fields = ('org', 'position')
+
+
+class BulletAdmin(admin.ModelAdmin):
+    list_display = ('experience', 'text', 'type')
+    list_filter = ('experience', 'type')
+    search_fields = ('experience', 'text')
+
+
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('org', 'degree', 'concentration')
+    list_filter = ('org', 'degree', 'concentration')
+    search_fields = ('org', 'degree', 'concentration')
+
+
+class CertProjectActivityAdmin(admin.ModelAdmin):
+    list_display = ('org', 'title', 'description', 'variety')
+    list_filter = ('org', 'title', 'variety')
+    search_fields = ('org', 'title', 'description')
+
+
+class LanguageFATSAdmin(admin.ModelAdmin):
+    list_display = ('name', 'proficiency')
+    list_filter = ('name', 'proficiency')
+    search_fields = ('name', 'proficiency')
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'state', 'country')
+    list_filter = ('name', 'city', 'state', 'country')
+    search_fields = ('name', 'city', 'state', 'country')
+
+
+class PositionAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class DegreeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class ConcentrationAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+class OverviewAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'phone')
+    list_filter = ('name', 'email', 'phone')
+    search_fields = ('name', 'email', 'phone')
+
 
 admin.site.register(Overview)
 admin.site.register(Organization)
