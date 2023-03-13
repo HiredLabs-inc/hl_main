d143f103f1245f6f6e7406a4da2678e9b0803a5f
Jeff Stock <jantonstock@gmail.com>
2023-03-13 14:37:57 -0700

    updated admin interface

--- releases/admin.py
+++ releases/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import App, Release, Note, Feedback
+
+
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name',)
+
+
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('release', 'text')
+    list_filter = ('release',)
+
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ('user', 'app', 'text', 'created')
+    list_filter = ('user', 'app', 'created')
+
+
+class ReleaseAdmin(admin.ModelAdmin):
+    list_display = ('app', 'major', 'minor', 'patch', 'date')
+    list_filter = ('app', 'date')
+
+
+admin.site.register(App, AppAdmin)
+admin.site.register(Release, ReleaseAdmin)
+admin.site.register(Note, NoteAdmin)
+admin.site.register(Feedback, FeedbackAdmin)
