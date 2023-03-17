cea93b52ac57174ba516ae7fdad99739069c0f16
Jeff Stock <jantonstock@gmail.com>
2023-03-17 13:03:01 -0700

    added create title and company buttons to job update view

--- cold_apply/views.py
+++ cold_apply/views.py
@@ -266,6 +266,7 @@ class JobUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['pk'] = self.kwargs['pk']
         context['now'] = timezone.now()
 
         return context
