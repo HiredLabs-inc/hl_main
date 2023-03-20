db150d24d573594a0e797e727334e7f13b70425d
Jeff Stock <jantonstock@gmail.com>
2023-03-20 11:41:31 -0700

    set MEDIA_ROOT and MEDIA_URL to handle uploaded files

--- hl_main/urls.py
+++ hl_main/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -27,4 +29,4 @@ urlpatterns = [
     path('cold_apply/', include('cold_apply.urls')),
     path('resume/', include('resume.urls')),
     path('releases/', include('releases.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # this only works in development
