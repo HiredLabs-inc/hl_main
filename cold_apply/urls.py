fd05b693dd3c43940eaa1725d2716e44865a8f47
Jeff Stock <jantonstock@gmail.com>
2023-03-21 12:30:46 -0700

    refactored

--- cold_apply/urls.py
+++ cold_apply/urls.py
@@ -4,27 +4,45 @@ from .views import ParticipantListView, PhaseListView, ParticipantDetailView, Jo
     JobCreateView, OrganizationCreateView, TitleCreateView, ConfirmCreateView, JobUpdateView, \
     TitleUpdateView, OrganizationUpdateView, delete_job, refresh_keywords, create_participant, update_participant
 
-app_name = 'cold_apply'  # TODO: Organize this better
+app_name = 'cold_apply'
+
+# Index: list of current participants and link to process overview
 urlpatterns = [
     path('', ParticipantListView.as_view(), name='index'),
     path('process/', PhaseListView.as_view(), name='process'),
+]
+
+# Participants CRU (no delete)
+urlpatterns += [
     path('add_participant/', create_participant, name='add_participant'),
     path('participants/<int:pk>/', ParticipantDetailView.as_view(), name='participant_detail'),
-    path('jobs/<int:pk>/', JobDetailView.as_view(), name='job_detail'),
-    path('participants/<int:pk>/add_job/', JobCreateView.as_view(), name='create_job'),
-    path('jobs/confirm_add/', ConfirmCreateView.as_view(), name='confirm_add_job'),
-    path('jobs/<int:pk>/update/', JobUpdateView.as_view(), name='update_job'),
-    path('jobs/confirm_update/', ConfirmCreateView.as_view(), name='confirm_update_job'),
     path('participants/<int:pk>/update/', update_participant, name='update_participant'),
     path('participants/confirm_update/', ConfirmCreateView.as_view(), name='confirm_update_participant'),
+    ]
+
+# Companies CU (no read or delete) TODO: Add delete
+urlpatterns += [
     path('add_company/', OrganizationCreateView.as_view(), name='create_company'),
     path('companies/confirm_add/', ConfirmCreateView.as_view(), name='confirm_add_company'),
     path('companiies/<int:pk>/update/', OrganizationUpdateView.as_view(), name='update_company'),
     path('companies/confirm_update/', ConfirmCreateView.as_view(), name='confirm_update_company'),
+]
+
+# Titles CU (no read or delete) TODO: Add delete
+urlpatterns += [
     path('titles/add_title/', TitleCreateView.as_view(), name='create_title'),
+    path('titles/confirm_add/', ConfirmCreateView.as_view(), name='confirm_add_title'),
     path('titles/<int:pk>/update/', TitleUpdateView.as_view(), name='update_title'),
     path('titles/confirm_update/', ConfirmCreateView.as_view(), name='confirm_update_title'),
-    path('titles/confirm_add/', ConfirmCreateView.as_view(), name='confirm_add_title'),
     path('titles/keywords_refresh/<int:pk>/', refresh_keywords, name='refresh_keywords'),
-    path('jobs/delete/<int:pk>/', delete_job, name='delete_job')
 ]
+# Jobs CRUD
+urlpatterns += [
+    path('participants/<int:pk>/add_job/', JobCreateView.as_view(), name='create_job'),
+    path('jobs/confirm_add/', ConfirmCreateView.as_view(), name='confirm_add_job'),
+    path('jobs/<int:pk>/', JobDetailView.as_view(), name='job_detail'),
+    path('jobs/<int:pk>/update/', JobUpdateView.as_view(), name='update_job'),
+    path('jobs/confirm_update/', ConfirmCreateView.as_view(), name='confirm_update_job'),
+    path('jobs/delete/<int:pk>/', delete_job, name='delete_job'),
+    ]
+
--- releases/urls.py
+++ releases/urls.py
@@ -5,21 +5,37 @@ from .views import AppCreateView, ReleasesListView, ReleaseDetailView, ReleaseCr
     FeedbackDashboardView
 
 app_name = 'releases'
+
+# Index: List of releases notes with most recent at the top.
 urlpatterns = [
     path('app/<int:app_pk>/releases', ReleasesListView.as_view(), name='index'),
+]
+
+# Releases and Notes CRU (no delete)
+urlpatterns += [
+    path('release/new', ReleaseCreateView.as_view(), name='release_create'),
+    path('release/<int:pk>/confirm', ConfirmCreateView.as_view(), name='confirm_create'),
+    path('release/<int:pk>', ReleaseDetailView.as_view(), name='release_detail'),
+    ]
+
+# Notes CU (no read; no delete)
+urlpatterns += [
+    path('release/<int:pk>/add_note', NoteCreateView.as_view(), name='note_create'),
+    path('note/confirm', ConfirmCreateView.as_view(), name='confirm_note_create'),
+    ]
+
+# app C (no read, update, delete)
+urlpatterns += [
+    path('app/new', AppCreateView.as_view(), name='app_create'),
+    ]
+
+# Feedback CRU (no delete) TODO: Add delete
+urlpatterns += [
+    path('feedback/submit', FeedbackCreateView.as_view(), name='submit_feedback'),
+    path('app/feedback/confirm', ConfirmCreateView.as_view(), name='confirm_submit_feedback'),
     path('app/<int:app_pk>/feedback', FeedbackDashboardView.as_view(), name='feedback_dashboard'),
     path('app/<int:app_pk>/feedback_admin', FeedbackListView.as_view(), name='feedback_list'),
-    path('app/feedback/confirm', ConfirmCreateView.as_view(), name='confirm_submit_feedback'),
-    path('app/new', AppCreateView.as_view(), name='app_create'),
-    path('release/<int:pk>', ReleaseDetailView.as_view(), name='release_detail'),
-    path('release/new', ReleaseCreateView.as_view(), name='release_create'),
-    path('release/<int:pk>/add_note', NoteCreateView.as_view(), name='note_create'),
-    path('release/<int:pk>/confirm', ConfirmCreateView.as_view(), name='confirm_create'),
-    path('feedback/submit', FeedbackCreateView.as_view(), name='submit_feedback'),
     path('feedback/<int:pk>', FeedbackDetailView.as_view(), name='feedback_detail'),
     path('<int:app_pd>/feedback/<int:pk>/update', FeedbackUpdateView.as_view(), name='feedback_update'),
     path('feedback/update/confirm', ConfirmCreateView.as_view(), name='confirm_update_feedback'),
-    path('note/confirm', ConfirmCreateView.as_view(), name='confirm_note_create'),
-    path('release/confirm', ConfirmCreateView.as_view(), name='confirm_release_create'),
-
 ]
