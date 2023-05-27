ea9cbf3712f30b2fc159e143d0fcc1a9ea995335
Redmond Scales <redmond.j.scales@gmail.com>
2023-05-27 11:49:12 +0100

    fixed education template

--- cold_apply/urls.py
+++ cold_apply/urls.py
@@ -160,7 +160,7 @@ urlpatterns += [
         name="confirm_add_education",
     ),
     path(
-        "participants/<int:participant_id>/edu/<int:pk>/delete/",
+        "education/<int:pk>/delete/",
         delete_education,
         name="delete_education",
     ),
