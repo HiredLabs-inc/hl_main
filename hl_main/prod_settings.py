132e6910c3e6e2e1658edb958c5f719410de3bd8
redmondscales <redmond.j.scales@gmail.com>
2023-06-17 17:37:17 +0100

    added breadcrumbs to builtins

--- hl_main/prod_settings.py
+++ hl_main/prod_settings.py
@@ -72,6 +72,9 @@ TEMPLATES = [
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
             ],
+            "builtins": [
+                "cold_apply.templatetags.breadcrumbs",
+            ],
         },
     },
 ]
